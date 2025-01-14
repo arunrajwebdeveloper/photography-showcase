778db4052024b1c9bca7324af514bc1cb6045e39
arunraj.r <arunraj.r@nergyindia.com>
2025-01-14 11:52:04 +0530

    showcase efx disabled

--- js/index.js
+++ js/index.js
@@ -201,32 +201,32 @@ document.addEventListener("DOMContentLoaded", function () {
   // *************************************************************************
   // *************************************************************************
 
-  if (document.querySelector(".image-grid-item")) {
-    let proxy = { skew: 0 },
-      skewSetter = gsap.quickSetter(".image-grid-item", "skewY", "deg"), // fast
-      clamp = gsap.utils.clamp(-20, 20); // don't let the skew go beyond 20 degrees.
+  // if (document.querySelector(".image-grid-item")) {
+  //   let proxy = { skew: 0 },
+  //     skewSetter = gsap.quickSetter(".image-grid-item", "skewY", "deg"), // fast
+  //     clamp = gsap.utils.clamp(-20, 20); // don't let the skew go beyond 20 degrees.
 
-    ScrollTrigger.create({
-      onUpdate: (self) => {
-        let skew = clamp(self.getVelocity() / -300);
-        if (Math.abs(skew) > Math.abs(proxy.skew)) {
-          proxy.skew = skew;
-          gsap.to(proxy, {
-            skew: 0,
-            duration: 0.8,
-            ease: "power3",
-            overwrite: true,
-            onUpdate: () => skewSetter(proxy.skew),
-          });
-        }
-      },
-    });
+  //   ScrollTrigger.create({
+  //     onUpdate: (self) => {
+  //       let skew = clamp(self.getVelocity() / -300);
+  //       if (Math.abs(skew) > Math.abs(proxy.skew)) {
+  //         proxy.skew = skew;
+  //         gsap.to(proxy, {
+  //           skew: 0,
+  //           duration: 0.8,
+  //           ease: "power3",
+  //           overwrite: true,
+  //           onUpdate: () => skewSetter(proxy.skew),
+  //         });
+  //       }
+  //     },
+  //   });
 
-    gsap.set(".image-grid-item", {
-      transformOrigin: "right center",
-      force3D: true,
-    });
-  }
+  //   gsap.set(".image-grid-item", {
+  //     transformOrigin: "right center",
+  //     force3D: true,
+  //   });
+  // }
 
   // REVEAL ELEMENT
 
